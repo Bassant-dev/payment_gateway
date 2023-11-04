5d03466f85360f72aded9616d89985a52fde4422
bassant <bassantmohamed501@gmail.com>
2023-11-04 22:15:50 +0200

    create api services

--- lib/core/utils/stripe_services.dart
+++ lib/core/utils/stripe_services.dart
@@ -1,3 +1,10 @@
-class Stripe{
+
+import 'package:payment_app/features/checkout/data/model/payment_intent_input_model/payment_intent_input_model.dart';
+import 'package:payment_app/features/checkout/data/model/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel paymentIntentInputModel){
+  //
+  // }
 
 }
